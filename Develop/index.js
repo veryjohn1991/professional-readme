e751bac647c18bab7b10bb55bbeaef4bbc18f240
very john <armaanjelly@gmail.com>
2023-12-09 12:28:14 -0800

    adding some questons

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: ' what is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: ' what is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const readmeContent = generateMarkdown(answers);
+
+            fs.writeFile('myreadme.md', readmeContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created myreadme.md')
+            );
+        });
+}
 
 // Function call to initialize app
 init();
